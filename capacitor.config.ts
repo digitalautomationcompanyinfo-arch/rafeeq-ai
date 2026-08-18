278c95cce846122c5fd38f16db00f34c575fd34c
albraedu-lgtm <albra.edu@gmail.com>
2026-08-18 13:31:04 -0700

    build: Add Capacitor for Android and iOS mobile app generation

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.learnov.rafeeq',
+  appName: 'Rafeeq Learnov',
+  webDir: 'dist'
+};
+
+export default config;
